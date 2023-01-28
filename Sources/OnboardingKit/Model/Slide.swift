374c8115f6e69832c41f76b3d48a25338ca7b286
Nelson Junior <nsilva.solks@digio.com.br>
2023-01-28 00:47:21 -0300

    Created Slide model

---
+++ Sources/OnboardingKit/Model/Slide.swift
@@ -0,0 +1,19 @@
+//
+//  Slide.swift
+//  
+//
+//  Created by Junior Silva on 27/01/23.
+//
+
+import Foundation
+import UIKit
+
+public struct Slide {
+    let image: UIImage
+    let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+}
